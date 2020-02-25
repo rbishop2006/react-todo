f6d831af3cd7de8eefb78878f417c84d2bbd4d33
Robert Bishop <rbishop2006@gmail.com>
2020-02-25 15:20:53 -0800

    initial bp

---
+++ src/redux/ducks/index.js
@@ -0,0 +1,8 @@
+// export all reducers from this file
+/*
+  ex:
+
+  export { deafult as userState } from './users'
+
+  were './users' is a duck
+*/
---
+++ src/redux/ducks/readme.js
@@ -0,0 +1,8 @@
+/*
+1. imports
+2. action definitions
+3. initial state
+4. reducer (default export)
+5. action creators
+6. custom hook (named export)
+*/
